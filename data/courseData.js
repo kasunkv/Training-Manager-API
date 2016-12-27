864c83a74ccc93d23481c83d84cd41bcc1cdbbde
kasunkv <kvkrusl@gmail.com>
2016-12-27 12:04:04 +0530

    Added the sample data for the MongoDb

---
+++ data/courseData.js
@@ -0,0 +1,43 @@
+db.courses.insert([
+{
+	title: 'Application Instrumentation Using Log4net',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Pluralsight',
+    tags: [".Net", "Logging"],
+	completed: false
+},
+{
+	title: 'Advanced JavaScript',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Pluralsight',
+    tags: ["JavaScript"],
+	completed: false
+},
+{
+	title: 'JavaScript Best Practices',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Pluralsight',
+    tags: ["JavaScript", "Patterns"],
+	completed: false
+},
+{
+	title: 'Agile Project Management',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Lynda.com',
+    tags: ["Agile", "Project Management"],
+	completed: false
+},
+{
+	title: 'Android SDK Essential Training',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Lynda.com',
+    tags: ["Android", "Java"],
+	completed: false
+},
+{
+	title: 'Bootstrap 3 Adding Interactivity to Your Site',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Lynda.com',
+    tags: ["Bootstrap", "CSS"],
+	completed: false
+}])
